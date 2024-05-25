9beb9009e0d1a02c3520e894a4f3f7d67c5dd62e
theycallmejeano <jeangithae@outlook.com>
2024-05-25 17:08:16 +0300

    final modification

--- src/app/books.py
+++ src/app/books.py
@@ -12,69 +12,64 @@ BOOKS = [
     {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
 ]
 
-@app.get("/api-endpoint")
-async def first_func():
-    return {'message' : 'hello Jean'}
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
+
+@app.get("/books/{book_title}")
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+    
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
 
 
-# @app.get("/books")
-# async def read_all_books():
-#     return BOOKS
+# Get all books from a specific author using path or query parameters
+@app.get("/books/byauthor/")
+async def read_books_by_author_path(author: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == author.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
 
 
-# @app.get("/books/{book_title}")
-# async def read_book(book_title: str):
-#     for book in BOOKS:
-#         if book.get('title').casefold() == book_title.casefold():
-#             return book
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and \
+                book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
 
 
-# @app.get("/books/")
-# async def read_category_by_query(category: str):
-#     books_to_return = []
-#     for book in BOOKS:
-#         if book.get('category').casefold() == category.casefold():
-#             books_to_return.append(book)
-#     return books_to_return
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
 
 
-# # Get all books from a specific author using path or query parameters
-# @app.get("/books/byauthor/")
-# async def read_books_by_author_path(author: str):
-#     books_to_return = []
-#     for book in BOOKS:
-#         if book.get('author').casefold() == author.casefold():
-#             books_to_return.append(book)
-
-#     return books_to_return
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
 
 
-# @app.get("/books/{book_author}/")
-# async def read_author_category_by_query(book_author: str, category: str):
-#     books_to_return = []
-#     for book in BOOKS:
-#         if book.get('author').casefold() == book_author.casefold() and \
-#                 book.get('category').casefold() == category.casefold():
-#             books_to_return.append(book)
-
-#     return books_to_return
-
-
-# @app.post("/books/create_book")
-# async def create_book(new_book=Body()):
-#     BOOKS.append(new_book)
-
-
-# @app.put("/books/update_book")
-# async def update_book(updated_book=Body()):
-#     for i in range(len(BOOKS)):
-#         if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
-#             BOOKS[i] = updated_book
-
-
-# @app.delete("/books/delete_book/{book_title}")
-# async def delete_book(book_title: str):
-#     for i in range(len(BOOKS)):
-#         if BOOKS[i].get('title').casefold() == book_title.casefold():
-#             BOOKS.pop(i)
-#             break
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
