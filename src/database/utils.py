61908e5fb275f92ad2f906121e59b3e1ac119aab
theycallmejeano <jeangithae@outlook.com>
2024-05-28 11:46:30 +0300

    basic function to connect to the DB

---
+++ src/database/utils.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+from pathlib import Path
+
+try:
+    env_file = find_dotenv()
+    load_dotenv(env_file)
+except OSError:
+    envp = Path.cwd().parent / '.env'
+    load_dotenv(dotenv_path=envp.as_posix())
+
+USER = os.environ.get("POSTGRES_USER")
+DB_NAME = os.environ.get('POSTGRES_DATABASE')
+PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+PORT = os.environ.get("POSTGRES_PORT")
+HOST = os.environ.get("POSTGRES_HOST")
+
+
+def get_db_properties():
+    postgres_db = {'drivername': 'postgresql',
+                       'username': USER,
+                       'password': PASSWORD,
+                       'host': HOST,
+                       'port': PORT,
+                       'database': DB_NAME}
+
+    return postgres_db
+
+
