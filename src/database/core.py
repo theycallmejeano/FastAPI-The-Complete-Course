146bd9ca4af2d68bafb99d84ae3524a61c86e95c
theycallmejeano <jeangithae@outlook.com>
2024-05-28 11:45:58 +0300

    DB constructor

--- src/database/core.py
+++ src/database/core.py
@@ -1,11 +1,50 @@
 from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from loguru import logger
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+class PostgresDB:
+    _instance = None
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    def __new__(cls, pg_creds: dict, postgres_schema: str, *args, **kwargs):
+        """Singleton constructor for PostgresDB"""
+        print(pg_creds)
 
-Base = declarative_base()
+        if cls._instance is None:
+            cls._instance = super(PostgresDB, cls).__new__(cls)
+            try:
+                # Create engine with PostgreSQL credentials
+                cls._instance._engine = create_engine(
+                    URL.create(**pg_creds),
+                    connect_args={"options": f"-csearch_path={postgres_schema}"}
+                )
+                cls._instance._Session = sessionmaker(bind=cls._instance._engine)
+                cls._instance.schema = postgres_schema
+
+                logger.info(f"Connected to {pg_creds.get('database')} at schema {postgres_schema}")
+            except Exception as e:
+                logger.error(f"Error connecting to PostgresDB: {e}")
+                cls._instance = None  # Reset instance on error
+
+        return cls._instance
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def get_connection(self):
+        """Establish database connection"""
+        return self._engine.connect()
+
+    def get_session(self):
+        """Create a new SQLAlchemy session"""
+        return self._Session()
+
+    def close(self):
+        """Dispose database engine"""
+        if self._engine:
+            self._engine.dispose()  # close out all connections
+            logger.info("Close all connections associated with the engine.")
